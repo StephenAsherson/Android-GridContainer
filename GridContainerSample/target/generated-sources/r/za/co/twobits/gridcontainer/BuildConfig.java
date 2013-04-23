91d44bc24a2e89fe2173f8aa53aa8c8e158ac0a7
Stephen Asherson <sasherson@gmail.com>
2013-04-23 11:44:44 +0200

    Updating ignore file to exclude Maven target files; Cleaning up target files in repo

--- GridContainerSample/target/generated-sources/r/za/co/twobits/gridcontainer/BuildConfig.java
+++
@@ -1,5 +0,0 @@
-package za.co.twobits.gridcontainer;
-
-public final class BuildConfig {
-  public static final boolean DEBUG = true;
-}
--- GridContainerSample/target/generated-sources/r/za/co/twobits/gridcontainersample/BuildConfig.java
+++
@@ -1,5 +0,0 @@
-package za.co.twobits.gridcontainersample;
-
-public final class BuildConfig {
-  public static final boolean DEBUG = true;
-}
